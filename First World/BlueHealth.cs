7b54af14601edd999dfb6f76c24262720badd637
Scott Killeen <scott.killeen@hotmail.com>
2015-03-21 16:31:54 -0400

    First World

---
+++ First World/BlueHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		GameObject Dwarf = GameObject.Find("Dwarf");
+		DwarfMovement dwarfScript = Dwarf.GetComponent<DwarfMovement>();
+		if(col.gameObject.name=="Dwarf"){
+			Destroy(gameObject);
+			if(dwarfScript.dwarfLife<=85f){
+				dwarfScript.dwarfLife=dwarfScript.dwarfLife+15f;
+			}
+			else{
+				dwarfScript.dwarfLife=100f;
+			}
+		}
+	}
+}
