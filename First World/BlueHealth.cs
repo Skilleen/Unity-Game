a636c42246d2c6fa786fb8134483ef33c9e59931
Scott Killeen <scott.killeen@hotmail.com>
2015-03-23 22:08:34 -0400

    First World

--- First World/BlueHealth.cs
+++ First World/BlueHealth.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/* Created by Scott Killeen, March 23rd, 2015.
+ * Methods for dealing with the health gem.
+ * */
 public class BlueHealth : MonoBehaviour {
 
 	// Use this for initialization
@@ -12,15 +14,16 @@ public class BlueHealth : MonoBehaviour {
 	void Update () {
 	
 	}
+	//When the player picks up health.
 	void OnCollisionEnter2D(Collision2D col){
 		GameObject Dwarf = GameObject.Find("Dwarf");
 		DwarfMovement dwarfScript = Dwarf.GetComponent<DwarfMovement>();
 		if(col.gameObject.name=="Dwarf"){
 			Destroy(gameObject);
-			if(dwarfScript.dwarfLife<=85f){
+			if(dwarfScript.dwarfLife<=85f){ //If Less then 85
 				dwarfScript.dwarfLife=dwarfScript.dwarfLife+15f;
 			}
-			else{
+			else{ //else just give him full health
 				dwarfScript.dwarfLife=100f;
 			}
 		}
