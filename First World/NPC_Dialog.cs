72f670bd06c29d21c9307159d24e05258cdc3a61
Scott Killeen <scott.killeen@hotmail.com>
2015-03-23 22:09:13 -0400

    First World

--- First World/NPC_Dialog.cs
+++ First World/NPC_Dialog.cs
@@ -6,16 +6,24 @@ using System.Collections;
  * */
 public class NPC_Dialog : MonoBehaviour {
 	public float range = 0; //check if the player is in range
-	public bool displayDialog = false;
+	public bool displayDialog = false; //If the conversation box is shown.
 	public Transform target;
+	public bool questAccept = false; //If the user accepted.
+	public Rigidbody2D rb;
 
 
 	
 	// Update is called once per frame
 	void Update () {
+		//If the player has moved away
 		range = Vector2.Distance(transform.position, target.position);
 		if(range >=5){
-		displayDialog=false;
+			displayDialog=false;
+			Cursor.visible=false;
+		}
+		rb = GetComponent<Rigidbody2D>();
+		if(rb.mass==2){
+			questAccept=true;
 		}
 	}
 
@@ -23,6 +31,7 @@ public class NPC_Dialog : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.name == "Dwarf"){
 			displayDialog=true;
+
 		}
 }
 }
