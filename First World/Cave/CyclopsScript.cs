f3edaab4bb29d5cf890b6198cf64d80d23c54a65
Scott Killeen <scott.killeen@hotmail.com>
2015-03-26 22:58:00 -0400

    Cave

---
+++ First World/Cave/CyclopsScript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+/* Scott Killeen, March 25th, 2015
+ * Methods for Dealing with the movements and animations of the Cyclops (Enemy)
+ * 
+ * */
+public class CyclopsScript : MonoBehaviour {
+	//Variables for getting the goblin to follow the Player and to react to animations.
+	public Transform target;
+	public float speed = 3f;
+	private float maxDistance = 30f;
+	private float attackDistance = 5f;
+	private float range;
+	private Animator anim;
+	//I usually avoid the use of global variables, but unity encourages it for working with the unity interface.
+	public bool cyclopsDead=false;
+	public bool doDamage=false; //if both below are true.
+	public bool playerHit=false; //If the cyclops hits the player
+	public bool inRange=false; //If player is in range
+	public float cyclopsLife=200f; 
+	public Rigidbody2D rb;
+	public bool facingRight=true;
+	
+	
+	void Start () { 
+		anim = GetComponent<Animator>();
+	}
+	
+	void FixedUpdate ()
+	{	
+		
+		doDamage=false;
+		playerHit=false;
+		//To find and move to the Player
+		float myposition = transform.position.x;
+		float dwarfposition = target.position.x;
+		if (dwarfposition >myposition && !facingRight){
+			Flip(); }
+		else if(myposition > dwarfposition && facingRight){
+			Flip();
+		}
+		range = Vector2.Distance(transform.position, target.position);
+		//If Player is in range, start moving towards.
+		if (range < maxDistance)
+		{
+			anim.SetBool("walk",true);
+			transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		}
+		else{
+			anim.SetBool("walk",false);
+		}
+		if (range > attackDistance){
+			anim.SetBool("attack",false);
+		}
+		if (range < 3f) {
+			if(anim.GetBool("attack")==true){
+				playerHit = true;
+				doDamage=true;}
+		}
+		//Death
+		if(cyclopsLife<=0){
+			transform.position = new Vector3(transform.position.x,transform.position.y,2); //move on the z.
+			anim.SetBool("dead",true);
+			anim.SetBool("attack", false);
+			anim.SetBool("walk", false);
+			speed = 0f;
+			cyclopsDead=true;
+			rb.isKinematic = true;
+			BoxCollider2D b; //Remove the collision box.
+			b = GetComponent<BoxCollider2D>();
+			b.enabled = false;
+		}
+	}
+	//Handles Collisions
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.name == "Dwarf" && !cyclopsDead){
+			anim.SetBool("attack",true);
+			if (range < attackDistance){
+				playerHit = true;
+			}
+			
+		}
+		
+	}
+	//To face left and right
+	void Flip ()
+	{
+		if(!cyclopsDead){
+			facingRight = !facingRight;		
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+	
+	
+}
---
+++ First World/Cave/ScoreCave.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/* Scott Killeen, March 24th, 2015
+ * Methods for the score and text.
+ * 
+ * */
+public class ScoreCave : MonoBehaviour {
+	public Text instruction;
+	public int armourLevel = 5;
+	public int strengthLevel = 10;
+	private int count = 0;
+	private bool firstEnter = true; //First time player entered the cave.
+	// Use this for initialization
+	void Start () {
+		instruction = GetComponent<Text>();
+		Cursor.visible = false;
+
+		//Enemy Yells when the player enters the cave.
+		if(firstEnter){
+			firstEnter=false;
+			instruction.text="                      \"YOU DIE NOW!\"";
+			count=100;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject Dwarf = GameObject.Find("Dwarf");
+		DwarfCave dwarfScript = Dwarf.GetComponent<DwarfCave>();
+		GameObject Armour = GameObject.Find("Armour");
+		ArmourScript armourScript = Armour.GetComponent<ArmourScript>();
+		//If the dwarf collides with the sign.
+
+		//If the dwarf collides with the cave.
+		if(dwarfScript.caveContact){
+			dwarfScript.caveContact=false;
+			count=200;
+		}
+		//If the dwarf collides with the axe.
+		else if(armourScript.armourContact){
+			armourScript.armourContact=false;
+			count=100;
+		}
+		else if(count==0){
+
+			instruction.text="Health: "+dwarfScript.dwarfLife.ToString("n2")+"   Armour:"+armourLevel+"    Strength:"+strengthLevel;
+		}
+		if(count>0){
+			count--;
+		}
+		armourScript.armourContact=false;
+	}
+}
