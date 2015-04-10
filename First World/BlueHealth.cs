b1c506a6f9b142ffae1da1fde227cb1496f547a4
Scott Killeen <scott.killeen@hotmail.com>
2015-04-10 00:42:37 -0400

    First World

--- First World/BlueHealth.cs
+++ First World/BlueHealth.cs
@@ -25,8 +25,8 @@ public class BlueHealth : MonoBehaviour {
 		//Health in the World one cave.
 		if(col.gameObject.name=="Dwarf" && level =="World One Cave"){
 			Destroy(gameObject);
-			if(dwarfScriptCave.dwarfLife<=70f){
-				dwarfScriptCave.dwarfLife=dwarfScriptCave.dwarfLife+30f; //If health is less than 30.
+			if(dwarfScriptCave.dwarfLife<=85f){
+				dwarfScriptCave.dwarfLife=dwarfScriptCave.dwarfLife+15f; //If health is less than 30.
 			}
 			else{
 				dwarfScriptCave.dwarfLife=100f; //Else just give the player full health.
